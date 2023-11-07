8b827266ae12f293ed8733fdac7c70910bf9c954
liz-garcia <lizgarciamarin@gmail.com>
2023-11-07 21:21:22 +0100

    Fetch to Weather API is working properly.

--- homework/HackYourTemperature/server.js
+++ homework/HackYourTemperature/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import { keys } from './sources/keys.js';
 
 const app = express();
 const port = 3000;
@@ -10,9 +11,26 @@ app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 
-app.post('/weather', (req, res) => {
+app.post('/weather', async (req, res) => {
   const cityName = req.body.cityName;
-  res.send(`You entered the city: ${cityName}`);
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keys.API_KEY}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.cod === '404') {
+      res.json({ weatherText: 'City is not found!' });
+    } else {
+      const cityName = data.name;
+      const temperature = data.main.temp;
+      res.json({ weatherText: `The temperature in ${cityName} is ${temperature}°C.` });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while fetching weather data.');
+  }
 });
 
 app.get('/weather', (req, res) => {
---
+++ homework/HackYourTemperature/sources/keys.js
@@ -0,0 +1,3 @@
+export const keys = {
+  API_KEY: "ae53cd06d57e9af22f200d160750d74c"
+};
